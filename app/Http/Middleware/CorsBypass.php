afb56c311915f9b2710eecc9687946d459e9023d
Gabriel Scatolin <gscatolin@outlook.com>
2021-12-16 01:13:42 -0300

    Ignore cors problems

--- app/Http/Middleware/CorsBypass.php
+++ app/Http/Middleware/CorsBypass.php
@@ -17,6 +17,7 @@ class CorsBypass
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');;
+            ->header('Access-Control-Allow-Headers', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }
