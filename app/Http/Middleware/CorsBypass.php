cc859561e78648ba73d0dc7171072e20b5469147
Gabriel Scatolin <gscatolin@outlook.com>
2021-12-16 01:25:21 -0300

    Ignore cors problems

--- app/Http/Middleware/CorsBypass.php
+++ app/Http/Middleware/CorsBypass.php
@@ -15,9 +15,6 @@ class CorsBypass
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Headers', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        return $next($request);
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -52,4 +52,8 @@ $app->singleton(
 |
 */
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
+
 return $app;
